c7d401c66c4db200d4923d58630c919259232e78
Aparna Mishra <aparna.m699@gmail.com>
2023-08-25 21:52:10 +0530

    commit 2: address get,post,delete and put

--- src/main/java/com/example/ecommerce/controller/AddressController.java
+++ src/main/java/com/example/ecommerce/controller/AddressController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,13 +44,34 @@ public class AddressController {
 				.orElseThrow(() -> new ResourceNotFoundException("user not found with id:"+ addressId));
 	}
 	
-//	//GET address by userID
-//	@GetMapping("api/address/user/{userId}")
-//	public List<Address> getAddressByUserId(@PathVariable(value = "userId") long  userId){
-//		User user = new User(userId);
-//		return ((List<Address>) this.addressRepository.findByUserId(user))
-//				.orElseThrow(() -> new ResourceNotFoundException("user not found with id:"+ userId));
-//	}
+	//GET address by userID
+	@GetMapping("api/address/user/{userId}")
+	public List<Address> getAddressByUserId(@PathVariable(value = "userId") long  userId){
+		User user = new User(userId);
+		return this.addressRepository.findByUserId(user);
+	}
+	
+	//PUT Edit Address
+	@PutMapping("api/address/{id}")
+	public Address updateAddress(@RequestBody Address address, @PathVariable(value = "id") long  addressId ) {
+		Address existingAddress = this.addressRepository.findById(addressId)
+				.orElseThrow(() -> new ResourceNotFoundException("user not found with id:"+ addressId));
+		existingAddress.setPinCode(address.getPinCode()!= null ? 
+				address.getPinCode() : existingAddress.getPinCode());
+		existingAddress.setUnit(address.getUnit()!= null ? 
+				address.getUnit() : existingAddress.getUnit());
+		existingAddress.setLine1(address.getLine1()!= null ? 
+				address.getLine1() : existingAddress.getLine1());
+		existingAddress.setLine2(address.getLine2()!= null ? 
+				address.getLine2() : existingAddress.getLine2());
+		existingAddress.setCity(address.getCity()!= null ? 
+				address.getCity() : existingAddress.getCity());
+		existingAddress.setState(address.getState()!= null ? 
+				address.getState() : existingAddress.getState());
+		existingAddress.setCountry(address.getCountry()!= null ? 
+				address.getCountry() : existingAddress.getCountry());
+		return this.addressRepository.save(existingAddress);
+	}
 	
 	//DELETE address 
 	@DeleteMapping("api/address/{id}")
